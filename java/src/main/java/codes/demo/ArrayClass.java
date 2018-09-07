eb1bc7d6b65e140abb590f71dd8a1b0210b53114
zhenxingliu <392588121@qq.com>
2018-09-07 15:50:56 +0800

    1231313

--- java/src/main/java/codes/demo/ArrayClass.java
+++ java/src/main/java/codes/demo/ArrayClass.java
@@ -1,13 +1,30 @@
 package codes.demo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class ArrayClass {
 
 	public static void main(String[] args) {
 		Collection collections = new ArrayList();
 
+
+		List l = new ArrayList();
+		String[] elementData = new String[]{"1", "2", "3", "4", "5"};
+		String[] e1 = new String[5];
+		int index = 1;
+		int numMoved = elementData.length - index - 1;
+		int len = elementData.length;
+		System.arraycopy(elementData, index + 1, e1, 0,
+				3);
+		System.arraycopy(elementData, index + 1, elementData, index,
+				numMoved);
+
+		//elementData[--len] = null;
+		System.out.println(Arrays.asList(elementData));
+		System.out.println(Arrays.asList(e1));
 
 	}
 
